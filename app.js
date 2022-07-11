247ff7dc95af300e873f5f669ae1768c3e54ee4f
sepideh Borazjani <sepide.bri@gmail.com>
2022-07-11 13:04:45 +0430

    Add play and pause function and spin matched to play and pause

---
+++ app.js
@@ -0,0 +1,48 @@
+const playsongBtn = document.querySelector(".circle-inner");
+const audio = document.querySelector("audio");
+const iconPlayPause = document.querySelector(".play-pause");
+const spinCircle = document.querySelector(".circle-outer");
+const slidingTagLiAfterStyle = document.createElement("style");
+
+playsongBtn.addEventListener("click", () => {
+  try {
+    if (iconPlayPause.classList.contains("fa-play")) {
+      iconPlayPause.classList.remove("fa-play");
+      iconPlayPause.classList.add("fa-pause");
+
+      slidingTagLiAfterStyle.innerHTML = `
+      .circle-outer::after{
+        display: block;
+        border-radius: 50%;
+        padding: 0.6em;
+        content:"";
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        height: 100%;
+        width: 100%;
+         animation: spinner 1s infinite ;
+        background:var(--lilac-color);
+        background: linear-gradient(90deg, var(--lilac-color) 35%, var(--pink-color) 100%); 
+    } 
+      `;
+      document.head.appendChild(slidingTagLiAfterStyle);
+      playSong();
+    } else {
+      iconPlayPause.classList.remove("fa-pause");
+      iconPlayPause.classList.add("fa-play");
+      slidingTagLiAfterStyle.innerHTML = "";
+      pauseSong();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+function playSong() {
+  audio.play();
+}
+
+function pauseSong() {
+  audio.pause();
+}
